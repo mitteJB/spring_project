a8c97dceb68bccb88f8bce0afff19860cf08ecb7
JB <junbeom6789@gmail.com>
2021-05-26 10:15:43 +0900

    InsertController, ListController, MyBookController, SalesController, TviewController 변경 ( ModelAndView -> Model)

--- src/main/java/pack/controller/SalesController.java
+++ src/main/java/pack/controller/SalesController.java
@@ -2,6 +2,7 @@ package pack.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,23 +11,32 @@ import pack.model.LineInter;
 
 @Controller
 public class SalesController {
-	
+
 	@Autowired
 	private LineInter lineInter;
 	
-	@RequestMapping("sales")
+	/*@RequestMapping("sales")
 	public ModelAndView salelist(@RequestParam("ad_date") String ad_date) {
 		String ab_date = ad_date.substring(1,11);
-		
 		return new ModelAndView("sales1", "sales", lineInter.DatepayPart(ab_date));
+	}*/
+
+//	ModelAndView -> Model
+	@RequestMapping("sales")
+	public String salelist(Model model, @RequestParam("ad_date") String ad_date) {
+		model.addAttribute("sales", lineInter.DatepayPart(ad_date.substring(1, 11)));
+		return "sales1";
 	}
 
-	
-	
-	 
-	@RequestMapping("saletot")
+	/*@RequestMapping("saletot")
 	public ModelAndView saletot() {
 		return new ModelAndView("saletot", "sales", lineInter.SalesDataAll());
+	}*/
+
+//	ModelAndView -> Model
+	@RequestMapping("saletot")
+	public String saletot(Model model) {
+		model.addAttribute("sales", lineInter.SalesDataAll());
+		return "saletot";
 	}
-	 
 }
